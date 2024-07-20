2aa964007426901adb2b163414dadb37ca716b60
Victoria <victoria.babchenko30@gmail.com>
2024-07-20 12:03:47 +0300

    added AddTwoPromises.js

--- AddTwoPromises.js
+++ AddTwoPromises.js
@@ -4,7 +4,7 @@
  * @return {Promise}
  */
 var addTwoPromises = async function(promise1, promise2) {
-    var [value1, value2] = await Promise.all([promise1, promise2]);
+    const [value1, value2] = await Promise.all([promise1, promise2]);
     return value1 + value2;
 };
 
