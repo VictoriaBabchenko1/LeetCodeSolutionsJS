722499c02947ff17f38416840b29b8ab5f2169af
Victoria <victoria.babchenko30@gmail.com>
2024-07-31 13:19:33 +0300

    added BestTimeToBuyAndSellStock.js

---
+++ BestTimeToBuyAndSellStock.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(prices) {
+    let minPrice = prices[0];
+    let profit = 0;
+
+    prices.forEach((price) => {
+        if(price < minPrice) {
+            minPrice = price;
+        }
+        else {
+            profit = Math.max(profit, price - minPrice);
+        }
+    })
+
+    return profit;
+};
--- ReverseInteger.js
+++ ReverseInteger.js
@@ -6,5 +6,3 @@ var reverse = function(x) {
     let reversed = parseInt(x.toString().split('').reverse().join('')) * Math.sign(x);
     return reversed >= Math.pow(-2, 31) && reversed <= Math.pow(2, 31) - 1 ? reversed : 0;
 };
-
-console.log(reverse(-123));
